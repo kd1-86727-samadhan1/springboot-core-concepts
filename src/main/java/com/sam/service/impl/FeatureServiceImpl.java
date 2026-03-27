0886e94dfb27cffb932644e457e8da85a86accad
Samadhan Aiwale <aiwalesamadhan16@gmail.com>
2026-03-27 09:44:13 +0530

    Step 12: Added FeatureService interface and implementation for conditional bean loading

---
+++ src/main/java/com/sam/service/impl/FeatureServiceImpl.java
@@ -0,0 +1,11 @@
+package com.sam.service.impl;
+
+import com.sam.service.FeatureService;
+
+public class FeatureServiceImpl implements FeatureService {
+	@Override
+	public String getFeatureMessage() {
+		// TODO Auto-generated method stub
+		return "Feature is ACTIVE - myapp.feature.enabled=true";
+	}
+}
