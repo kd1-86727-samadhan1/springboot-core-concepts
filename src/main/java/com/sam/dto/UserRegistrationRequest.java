dd054e095c2a397dc2202c98cd0049ccab0f9449
Samadhan Aiwale <aiwalesamadhan16@gmail.com>
2026-03-27 08:40:31 +0530

    Step 8: Added UserRegistrationRequest DTO with Bean Validation

---
+++ src/main/java/com/sam/dto/UserRegistrationRequest.java
@@ -0,0 +1,28 @@
+package com.sam.dto;
+
+import com.sam.validation.ValidPassword;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegistrationRequest {
+
+	@NotBlank(message = "Name cannot be blank")
+	private String name;
+
+	@NotBlank(message = "Email cannot be blank")
+	@Email(message = "Please enter valid email")
+	private String email;
+
+	@NotBlank(message = "Password cannot be blank")
+	@Size(min=8, message = "Password must be at least 8 characters")
+	@ValidPassword
+	private String password;
+}
